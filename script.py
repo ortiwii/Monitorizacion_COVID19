afa0bde385fee81561f501fc02fb59268391fd12
Gorka <ortiwii@gmail.com>
2022-01-06 00:19:26 +0100

    script v3

--- script.py
+++ script.py
@@ -1,6 +1,9 @@
 from datetime import date, timedelta
 from google.cloud import storage
 import pandas as pd
+from google.cloud import bigquery
+from datetime import datetime
+
 
 def upload_to_bucket(blob_name, path_to_file, bucket_name):
     """ Upload data to a bucket"""
@@ -16,23 +19,54 @@ def upload_to_bucket(blob_name, path_to_file, bucket_name):
 
     return blob.public_url
 
+def bigqueryExequte(rows_to_insert):
+    client = bigquery.Client.from_service_account_json(
+        'credentials.json')
+
+    table_id = "covid-336715.datoscovid.datos"
+
+    errors = client.insert_rows_json(table_id, rows_to_insert)  # Make an API request.
+
+    if errors == []:
+        print("New rows have been added.")
+    else:
+        print("Encountered errors while inserting rows: {}".format(errors))
+    client.close()
 if __name__ == "__main__":
 
-    yesterday = (date.today() - timedelta(days=1)).strftime('%m-%d-%Y')
-    yesterdayOK = (date.today() - timedelta(days=1)).strftime('%Y-%m-%d')
-    days_28 = (date.today() - timedelta(days=29)).strftime('%Y-%m-%d')
-    days_14 = (date.today() - timedelta(days=15)).strftime('%Y-%m-%d')
-    days_7 = (date.today() - timedelta(days=8)).strftime('%Y-%m-%d')
-    nombre = str(yesterday)+'.csv'
-    url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'+nombre
-    df = pd.read_csv(url)
-    df['Fecha'] = yesterday
-    df.to_csv(nombre, index = False)
-    u = upload_to_bucket(nombre, nombre, "covidinfo-bucket")
+        # YYYY - MM - DD   HH: MM[:SS[.SSSSSS]]"}
+        yesterday = (date.today() - timedelta(days=4)).strftime('%m-%d-%Y')
+        day = datetime.today() - timedelta(days=4)
+        yesterdayOK = (date.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+        days_28 = (date.today() - timedelta(days=29)).strftime('%Y-%m-%d')
+        days_14 = (date.today() - timedelta(days=15)).strftime('%Y-%m-%d')
+        days_7 = (date.today() - timedelta(days=8)).strftime('%Y-%m-%d')
+        print(yesterdayOK)
+        print(days_28)
+        print(days_14)
+        print(days_7)
+        nombre = str(yesterday)+'.csv'
+        url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'+nombre
+        df = pd.read_csv(url)
+        df.dropna(subset=["Lat"], inplace=True)
+        df = df.drop(columns=['FIPS', 'Admin2', 'Recovered', 'Active', 'Lat', 'Long_', 'Combined_Key'])
+        var = df['Incident_Rate'].iloc[0]
+        df['Incident_Rate'] = df['Incident_Rate'].round(7)
+        df['Case_Fatality_Ratio'] = df['Case_Fatality_Ratio'].round(7)
+        df['Fecha'] = str(day)
+        df = df.where(pd.notnull(df), None)
+        rows_to_insert = df.to_dict('records')
+        df.to_csv(nombre, index = False)
+        print(day)
 
-    jsonString = '{"arguments" : [{"name": "filename", "value": "gs://covidinfo-bucket/'+nombre+'"}, {"name": "day", "value": "'+str(yesterdayOK)+'"}, {"name": "day28", "value": "'+str(days_28)+'"}, {"name": "day14", "value": "'+str(days_14)+'"}, {"name": "day7", "value": "'+str(days_7)+'"}]}'
-    jsonFile = open("args.json", "w")
-    jsonFile.write(jsonString)
-    jsonFile.close()
 
-    u = upload_to_bucket("args.json", "args.json", "covidinfo-bucket")
+        u = upload_to_bucket(nombre, nombre, "covidinfo-bucket")
+
+        jsonString = '{"arguments" : [{"name": "filename", "value": "gs://covidinfo-bucket/'+nombre+'"}, {"name": "day", "value": "'+str(yesterdayOK)+'"}, {"name": "day28", "value": "'+str(days_28)+'"}, {"name": "day14", "value": "'+str(days_14)+'"}, {"name": "day7", "value": "'+str(days_7)+'"}]}'
+        jsonFile = open("args.json", "w")
+        jsonFile.write(jsonString)
+        jsonFile.close()
+
+        u = upload_to_bucket("args.json", "args.json", "covidinfo-bucket")
+
+        bigqueryExequte(rows_to_insert)
