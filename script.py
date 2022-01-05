73a8fc4391a8f546c7e49a9828d05779d18d5193
Gorka <ortiwii@gmail.com>
2022-01-05 18:38:52 +0100

    script v2

--- script.py
+++ script.py
@@ -19,15 +19,18 @@ def upload_to_bucket(blob_name, path_to_file, bucket_name):
 if __name__ == "__main__":
 
     yesterday = (date.today() - timedelta(days=1)).strftime('%m-%d-%Y')
+    yesterdayOK = (date.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+    days_28 = (date.today() - timedelta(days=29)).strftime('%Y-%m-%d')
+    days_14 = (date.today() - timedelta(days=15)).strftime('%Y-%m-%d')
+    days_7 = (date.today() - timedelta(days=8)).strftime('%Y-%m-%d')
     nombre = str(yesterday)+'.csv'
     url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'+nombre
     df = pd.read_csv(url)
     df['Fecha'] = yesterday
     df.to_csv(nombre, index = False)
-
     u = upload_to_bucket(nombre, nombre, "covidinfo-bucket")
 
-    jsonString = '{"arguments" : [{"name": "filename", "value": "gs://covidinfo-bucket/'+nombre+'"}, {"name": "day", "value": "'+str(yesterday)+'"}]}'
+    jsonString = '{"arguments" : [{"name": "filename", "value": "gs://covidinfo-bucket/'+nombre+'"}, {"name": "day", "value": "'+str(yesterdayOK)+'"}, {"name": "day28", "value": "'+str(days_28)+'"}, {"name": "day14", "value": "'+str(days_14)+'"}, {"name": "day7", "value": "'+str(days_7)+'"}]}'
     jsonFile = open("args.json", "w")
     jsonFile.write(jsonString)
     jsonFile.close()
